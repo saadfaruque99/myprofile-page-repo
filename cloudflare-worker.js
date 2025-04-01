a84d91c51bcca959b36b3f71e977f05b50297ed0
Saad Faruque <faruque@gmail.com>
2025-04-01 23:25:40 +0600

    Improved Content Security Policy by removing unsafe directives

--- cloudflare-worker.js
+++ cloudflare-worker.js
@@ -11,7 +11,7 @@ async function handleRequest(request) {
   const newResponse = new Response(response.body, response)
   
   // Add security headers
-  newResponse.headers.set('Content-Security-Policy', "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' data:; connect-src 'self'; frame-ancestors 'none'")
+  newResponse.headers.set('Content-Security-Policy', "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; font-src 'self' data:; connect-src 'self'; frame-ancestors 'none'; object-src 'none'; base-uri 'self'")
   newResponse.headers.set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload')
   newResponse.headers.set('X-Frame-Options', 'DENY')
   newResponse.headers.set('X-Content-Type-Options', 'nosniff')
